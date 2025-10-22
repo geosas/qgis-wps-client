2309741ed39705a9c39e86e5fa1792e35345fe51
hsquividant <herve.squividant@agrocampus-ouest.fr>
2025-10-22 13:01:42 +0200

    Updated for PyQt5+ compatibility.
    Seems to work with at least version 3.44 of QGis.

--- apicompat/apicompat.py
+++ apicompat/apicompat.py
@@ -8,29 +8,49 @@
         begin                : 2013-07-02
         copyright            : (C) 2013 by Pirmin Kalberer, Sourcepole
         email                : pka@sourcepole.ch
+
+        Modified             : 2025-10-22 by [Herve Squividant]
+        Description          : Updated for PyQt5+ compatibility. 
+                               Seems to work with at least version 3.44 of QGis.
  ***************************************************************************/
 
 /***************************************************************************
  *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
+ *   This program is free software; you can redistribute it and/or modify *
+ *   it under the terms of the GNU General Public License as published by *
+ *   the Free Software Foundation; either version 2 of the License, or    *
  *   (at your option) any later version.                                   *
  *                                                                         *
  ***************************************************************************/
 """
+
 from __future__ import absolute_import
 
 from future import standard_library
 standard_library.install_aliases()
-import sip
+
 import builtins
+import qgis.PyQt.QtCore  # Ensures PyQt5/Qt environment is available
 
+# -----------------------------------------------------------------------------
+# Function: sipv1
+# Purpose: Detect whether the legacy SIP API v1 is in use (used in PyQt4)
+# In PyQt5 and later, sip.getapi('QVariant') raises a ValueError because
+# 'QVariant' API no longer exists. We catch the exception to handle this gracefully.
+# -----------------------------------------------------------------------------
 def sipv1():
-    return sip.getapi("QVariant") == 1
+    try:
+        import sip
+        # Only available in PyQt4; will raise ValueError in PyQt5+
+        return sip.getapi("QVariant") == 1
+    except (AttributeError, ValueError):
+        # In PyQt5+, assume SIP API v2 is active
+        return False
 
+# Make the function globally available to other plugin modules
 builtins.sipv1 = sipv1
 
+# Import appropriate compatibility layer depending on SIP API version
 if sipv1():
     from .sipv1 import compat
     from .sipv1 import vectorapi
